68e9410eb07677d78026069d3e8c3863904ae57b
cz <1024638031@qq.com>
2019-10-05 18:07:28 +0800

    提交到远程仓库

---
+++ src/main/java/com/springboot/springboot/Controller.java
@@ -0,0 +1,13 @@
+package com.springboot.springboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/hello")
+    public String hello(){
+      return "hello springboot";
+    }
+}
